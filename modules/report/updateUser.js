bf61f082b0d3472a8187949873049f2511cf35c3
nenravitsa <melioraa@gmail.com>
2018-10-06 13:15:16 +0300

    /

--- modules/report/updateUser.js
+++ modules/report/updateUser.js
@@ -5,6 +5,7 @@ const updateUser = (user, parse, id, username, squad, bot) => {
   //add new warrior to squad
   if(!user.squad){updateWarrior(id, 'squad', squad)}
   //update warrior info if it's necessary
+  //if(username)
   if(user.lvl!==parse.lvl){updateWarrior(id, 'lvl', parse.lvl)}
   if(user.attack!==parse.attack){updateWarrior(id, 'attack', parse.attack)}
   if(user.protec!==parse.protec){updateWarrior(id, 'protec', parse.protec)}
